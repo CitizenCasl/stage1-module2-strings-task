18ed75bc7223d1500b20a29df8ffa2f36b6e12ad
Sergey Gutsev <296529795@mail.ru>
2025-04-28 01:21:17 +0300

    Strings tasks

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,25 @@
 package com.epam.mjc;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 
 public class StringSplitter {
 
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        StringBuilder strDelimiters = new StringBuilder();
+        for (String delimiter : delimiters) {
+            strDelimiters.append(Pattern.quote(delimiter));
+        }
+        String[] strings = source.split("[" + strDelimiters + "]");
+        return Arrays.stream(strings).filter(x -> !x.isBlank()).map(String::trim).collect(Collectors.toList());
     }
 }
